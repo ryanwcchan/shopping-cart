83c5239453434c957c574a081ee3399f23a96393
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-10-16 20:22:08 -0400

    Setup testing dependencies

---
+++ tests/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import App from '../src/App'
+
+describe('App', () => {
+    it('renders hello world', () => {
+        render(<App />);
+
+        expect(screen.getByText(/Hello World/)).toBeInTheDocument();
+    })
+})
