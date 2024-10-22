bcd20b34e1af74c1466f44c69b8248c2a972578a
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-10-21 20:09:39 -0400

    Add styling to cart item and buttons

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,20 +5,17 @@ export default function CartItem({ productName, productId, quantity, productImg,
     <div className={style.itemContainer}>
       <img className={style.productImg} src={productImg} alt="" />
       <p className={style.productName}>{productName}</p>
-      <div 
-        className={style.quantity}
-      >
-        Qty: 
+      <div className={style.quantity}>
+        <span className={style.qty}>Qty:</span> 
         {quantity === 1 ? (
           <i className={`${style.button} fa-solid fa-trash`} onClick={() => deleteCartItem(productId)}></i>
         ) : (
           <i onClick={() => decreaseQuantity(productId)} className={`${style.button} fa-solid fa-minus`}></i>
         )}
-          
-            <p className={style.qtyDisplay}>{quantity}</p>
-          <i onClick={() => incrementQuantity(productId)} className={`${style.button} fa-solid fa-plus`}></i>
+        <p className={style.qtyDisplay}>{quantity}</p>
+        <i onClick={() => incrementQuantity(productId)} className={`${style.button} fa-solid fa-plus`}></i>
       </div>
-      <button className={style.deletBtn} onClick={() => deleteCartItem(productId)}>Delete</button>
+      <button className={style.deleteBtn} onClick={() => deleteCartItem(productId)}>Delete</button>
     </div>
   )
 }
