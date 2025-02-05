3af67f64b4193e8caa94a03e4dcf139e48c588b3
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-02-05 17:41:51 -0500

    style: Revamp style of cart page and cart item by using tables to display cart items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,37 +1,84 @@
-import style from './CartItem.module.css'
-import { useOutletContext } from 'react-router-dom'
-import QtyInput from './QtyInput'
-import { useState } from 'react'
+import style from "./CartItem.module.css";
+import { useOutletContext } from "react-router-dom";
+import QtyInput from "./QtyInput";
 
-export default function CartItem({ productName, productId, quantity, productImg, deleteCartItem, incrementQuantity, decreaseQuantity }) {
-  const { handleProductClick, editQuantity } = useOutletContext()
+export default function CartItem({
+  productName,
+  productId,
+  quantity,
+  productImg,
+  deleteCartItem,
+  incrementQuantity,
+  decreaseQuantity,
+  price,
+}) {
+  const { handleProductClick, editQuantity } = useOutletContext();
 
   return (
-    <div className={style.itemContainer}>
-      <img onClick={() => handleProductClick(productId)} className={style.productImg} src={productImg} alt="" />
-      <p onClick={() => handleProductClick(productId)} className={style.productName}>{productName}</p>
-      <div className={style.buttonWrapper}>
-        <div className={style.quantity}>
-          <span className={style.qty}>Qty:</span>
-          {quantity === 1 ? (
-            <i className={`${style.button} fa-solid fa-trash`} onClick={() => deleteCartItem(productId)}></i>
-          ) : (
-            <i onClick={() => decreaseQuantity(productId)} className={`${style.button} fa-solid fa-minus`}></i>
-          )}
-          <p className={style.qtyDisplay}>
-            <QtyInput 
-              quantity={quantity}
-              productId={productId}
-              editQuantity={editQuantity}
+    <tr key={productId}>
+      {/* Product column (Image and Name) */}
+      <tr className={style.productCell}>
+        <td>
+          <div>
+            <img
+              onClick={() => handleProductClick(productId)}
+              className={style.productImg}
+              src={productImg}
+              alt="Product image"
             />
-          </p>
-          <i onClick={() => incrementQuantity(productId)} className={`${style.button} fa-solid fa-plus`}></i>
+          </div>
+        </td>
+        <td>
+          <div className={style.nameContainer}>
+            <p
+              onClick={() => handleProductClick(productId)}
+              className={style.productName}
+            >
+              {productName}
+            </p>
+            <button
+              className={style.deleteBtn}
+              onClick={() => deleteCartItem(productId)}
+            >
+              Remove
+            </button>
+          </div>
+        </td>
+      </tr>
+
+      {/* Quantity column */}
+      <td className={style.priceContainer}>
+        <div className={style.buttonWrapper}>
+          <div className={style.quantity}>
+            <button
+              onClick={() => decreaseQuantity(productId)}
+              className={`${style.button} fa-solid fa-minus`}
+              disabled={quantity === 1}
+            ></button>
+            <p className={style.qtyDisplay}>
+              <QtyInput
+                quantity={quantity}
+                productId={productId}
+                editQuantity={editQuantity}
+              />
+            </p>
+            <button
+              onClick={() => incrementQuantity(productId)}
+              className={`${style.button} fa-solid fa-plus`}
+            ></button>
+          </div>
         </div>
-        <button className={style.deleteBtn} onClick={() => deleteCartItem(productId)}>
-          <i className='fa-solid fa-trash' />
-          <span className={style.deleteText}>Delete</span>
-        </button>
-      </div>
-    </div>
-  )
+      </td>
+
+      {/* Price column */}
+      <td>
+        <p className={style.price}>${price}</p>
+      </td>
+
+      {/* Total column */}
+      <td>
+        <p className={style.total}>${price * quantity}</p>
+      </td>
+    </tr>
+  );
 }
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,19 +1,25 @@
 import { useOutletContext } from "react-router-dom";
 import CartItem from "../components/CartItem";
-import style from './CartPage.module.css'
-import cartStyle from '../components/CartItem.module.css'
+import style from "./CartPage.module.css";
+import cartStyle from "../components/CartItem.module.css";
 import { useEffect, useState } from "react";
 
 export default function CartPage() {
-  const { cartData = [], emptyCart, deleteCartItem, incrementQuantity, decreaseQuantity } = useOutletContext();
+  const {
+    cartData = [],
+    emptyCart,
+    deleteCartItem,
+    incrementQuantity,
+    decreaseQuantity,
+  } = useOutletContext();
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    updateTotalPrice(cartData)
-  })
+    updateTotalPrice(cartData);
+  });
 
   function calculateTotal(items) {
-    return items.reduce((total, item) => total + item.price * item.quantity, 0)
+    return items.reduce((total, item) => total + item.price * item.quantity, 0);
   }
 
   function updateTotalPrice() {
@@ -22,26 +28,50 @@ export default function CartPage() {
 
   return (
     <div className={style.wrapper}>
-      <div className={style.cartInfo}>
-        <button onClick={emptyCart} className={cartStyle.deleteBtn}>Empty Cart</button>
-        <p className={style.totalPrice}>${totalPrice.toFixed(2)}</p>
-      </div>
-      <div className={style.cartContainer}>
-        {cartData.map((item) => {
-          return (
-            <CartItem
-              key={item.productId}
-              productId={item.productId}
-              productImg={item.image}
-              productName={item.productName}
-              quantity={item.quantity}
-              deleteCartItem={deleteCartItem}
-              incrementQuantity={incrementQuantity}
-              decreaseQuantity={decreaseQuantity}
-            />
-          )
-        })}
+      <div className={style.container}>
+        <div className={style.cartTableContainer}>
+          <table className={style.cartTable}>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartData.map((item) => {
+                return (
+                  <CartItem
+                    key={item.productId}
+                    productId={item.productId}
+                    productImg={item.image}
+                    productName={item.productName}
+                    quantity={item.quantity}
+                    deleteCartItem={deleteCartItem}
+                    incrementQuantity={incrementQuantity}
+                    decreaseQuantity={decreaseQuantity}
+                    price={item.price}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        {/* Order Summary */}
+        <div className={style.orderSummary}>
+          <h1>Order Summary</h1>
+
+          <div className={style.cartInfo}>
+            <button onClick={emptyCart} className={cartStyle.deleteBtn}>
+              Empty Cart
+            </button>
+            <p className={style.totalPrice}>
+              Subtotal: ${totalPrice.toFixed(2)}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
