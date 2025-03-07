e099f56da5e314b83455137d479693bdad0e0a30
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-03-07 14:38:17 -0500

    fix: Mobile responsive issuses for Header, Footer, ProductPage and CartPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className={style.container}>
       <div className={style.imgContainer}>
-        {/* <h1>Browse Top Quality Clothing</h1> */}
+        <h1>New clothing collection!</h1>
         <img
           src="https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="homepage image"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,16 @@ import style from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <div>Ryan Chan</div> | <a href="mailto:ryanwcchan@gmail.com">Email</a> |
-      <a href="https://linkedin.com/in/ryanwcchan" target="_blank">
-        LinkedIn
-      </a>
-      |
-      <a href="https://github.com/ryanwcchan" target="_blank">
-        GitHub
-      </a>
+      <div>Copyright © 2024 - Made by Ryan Chan</div>
+      <div className={style.links}>
+        <a href="https://linkedin.com/in/ryanwcchan" target="_blank">
+          LinkedIn
+        </a>
+        |
+        <a href="https://github.com/ryanwcchan" target="_blank">
+          GitHub
+        </a>
+      </div>
     </footer>
   );
 }
